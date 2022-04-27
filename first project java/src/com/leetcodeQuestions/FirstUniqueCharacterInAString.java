8c05128409758afeee56c97596d323a8027e86f2
dhruvmaindola227 <maindola.dhruv@gmail.com>
2022-04-27 19:20:12 +0530

    twosumII added

---
+++ first project java/src/com/leetcodeQuestions/FirstUniqueCharacterInAString.java
@@ -0,0 +1,30 @@
+package com.leetcodeQuestions;
+
+import java.util.HashMap;
+
+public class FirstUniqueCharacterInAString {
+    public static void main(String[] args) {
+        String s="aaabccd";
+        System.out.println(firstUniqChar(s));
+    }
+
+        public static int firstUniqChar(String s){
+            HashMap<Character,Integer> map = new HashMap<>();
+            for(int i=0;i<s.length();i++){
+                if(!map.containsKey(s.charAt(i))){
+                    map.put(s.charAt(i),1);
+                }else{
+                    map.put(s.charAt(i),map.get(s.charAt(i))+1);
+                }
+            }
+
+            for(int i=0;i<s.length();i++){
+                if(map.get(s.charAt(i))==1){
+                    return i;
+                }
+            }
+            return -1;
+
+        }
+    }
+
