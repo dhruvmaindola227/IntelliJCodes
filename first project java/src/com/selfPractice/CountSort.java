096c37aef38150c228398051b348be882391ac6a
dhruvmaindola227 <maindola.dhruv@gmail.com>
2022-05-06 07:54:21 +0530

    added changes

--- first project java/src/com/selfPractice/CountSort.java
+++ first project java/src/com/selfPractice/CountSort.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class CountSort {
     public static void main(String[] args) {
-        int[] arr= {0,0,1,1,1,11,2,1,11,0};
+        int[] arr= {1,0,1,2,2};
         System.out.println(Arrays.toString(countSort(arr)));
     }
 
@@ -12,22 +12,24 @@ public class CountSort {
         int max=0;
         int sum=0;
         int[] outputArr = new int[arr.length];
-        for(int i=0;i<arr.length;i++){
-            if(arr[i] > max){
-                max = arr[i];
+        for (int j : arr) {
+            if (j > max) {
+                max = j;
             }
         }
         int[] freqArr = new int[max+1];
         int[] sumArr = new int[freqArr.length];
         for(int i=0;i<arr.length;i++){
-            freqArr[arr[i]]+=1;
+            freqArr[arr[i]]++;
         }
+
         for(int i = 0 ; i < freqArr.length ; i++){
             sum+= freqArr[i];
             sumArr[i] = sum;
         }
+
         for(int i=0;i<arr.length;i++){
-            sumArr[arr[i]]-=1;
+            sumArr[arr[i]]--;
             outputArr[sumArr[arr[i]]]=arr[i];
         }
 
