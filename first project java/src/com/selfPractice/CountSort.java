a01c89bf9461a5820b2eb7949a8dcb23f235d1fe
dhruvmaindola227 <maindola.dhruv@gmail.com>
2022-05-07 23:00:48 +0530

    modified better count sort given by rohan

--- first project java/src/com/selfPractice/CountSort.java
+++ first project java/src/com/selfPractice/CountSort.java
@@ -4,9 +4,12 @@ import java.util.Arrays;
 
 public class CountSort {
     public static void main(String[] args) {
-        int[] arr= {1,0,1,2,2};
-        System.out.println(Arrays.toString(countSort(arr)));
+        int[] arr= {3,7,23,6,2,5};
+//        System.out.println(Arrays.toString(countSort(arr)));
+        countSort2(arr);
+        System.out.println(Arrays.toString(arr));
     }
+
 
     public static int[] countSort(int[] arr) {
         int max=0;
@@ -34,5 +37,33 @@ public class CountSort {
         }
 
         return outputArr;
+    }
+
+    public static void countSort2(int[] arr){
+        int max=0;
+        int sum=0;
+        int[] outputArr = new int[arr.length];
+        for (int j : arr) {
+            if (j > max) {
+                max = j;
+            }
+        }
+        int[] freqArr = new int[max+1];
+        int[] sumArr = new int[freqArr.length];
+        for(int i=0;i<arr.length;i++){
+            freqArr[arr[i]]++;
+        }
+        int j = 0;
+        int i = 0;
+        while(j  < freqArr.length){
+            if(freqArr[j] > 0){
+                arr[i] = j;
+                freqArr[j]--;
+                i++;
+            }else{
+                j++;
+            }
+        }
+
     }
 }
