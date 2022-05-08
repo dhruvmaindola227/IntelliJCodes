fb59c9d7739e6576e609d11ad6925a935499c775
dhruvmaindola227 <maindola.dhruv@gmail.com>
2022-05-08 09:53:00 +0530

    Radix sort added

---
+++ first project java/src/com/selfPractice/RadixSort.java
@@ -0,0 +1,89 @@
+package com.selfPractice;
+
+import java.util.Arrays;
+
+public class RadixSort {
+    public static void main(String[] args) {
+        int[] arr = {3425,344,12,0,234,296,234,14,556,564,56,5,345345,764635,254653};
+        System.out.println("answer array " + Arrays.toString(radixSort(arr)));
+
+    }
+    public static int[] radixSort(int[] arr) {
+        int numberOfDigitsInMax = 0;
+        int sum = 0;
+        int currentDigit=0;
+        int[] outputArr = new int[arr.length];
+        int[] freqArr = new int[10], sumArr = new int[10];
+        int max = arr[0];
+        for (int j : arr) {
+            if (j > max) {
+                max = j;
+            }
+        }
+        while(max != 0){
+            numberOfDigitsInMax++;
+            max/=10;
+        }
+
+        for (int i = 0; i < numberOfDigitsInMax; i++) {
+            sum = 0;
+            Arrays.fill(sumArr , 0);
+            Arrays.fill(freqArr , 0);
+            Arrays.fill(outputArr, 0);
+            for(int j = 0 ; j < arr.length ; j++){
+                currentDigit =(int) (arr[j] / Math.pow( 10, i)) % 10;
+//                System.out.println("Number is "+ arr[j] + " for i = " + i + " num is "+currentDigit);
+                freqArr[currentDigit]+=1;
+            }
+//            System.out.println("freq "+Arrays.toString(freqArr));
+
+            for (int j = 0; j < freqArr.length; j++) {
+                sum += freqArr[j];
+                sumArr[j] = sum;
+            }
+//            System.out.println("sum "+Arrays.toString(sumArr));
+
+            for (int j = arr.length-1; j >= 0; j--) {
+                currentDigit =(int) (arr[j] / Math.pow( 10, i)) % 10;
+                sumArr[currentDigit]--;
+                outputArr[sumArr[currentDigit]] = arr[j];
+            }
+            arr= Arrays.copyOf(outputArr , outputArr.length);
+
+        }
+        return arr;
+    }
+
+
+//    public static int[] radixSort2(int[] arr){
+//        int numberOfDigitsInMax = 0;
+//        int sum = 0;
+//        int currentDigit=0;
+//        int[] outputArr = new int[arr.length];
+//        int[] freqArr = new int[10], sumArr = new int[10];
+//        int max = arr[0];
+//        for (int j : arr) {
+//            if (j > max) {
+//                max = j;
+//            }
+//        }
+//        while(max != 0){
+//            numberOfDigitsInMax++;
+//            max/=10;
+//        }
+//
+//        for(int i = 0 ; i < numberOfDigitsInMax ; i++){
+//            sum = 0;
+//            Arrays.fill(sumArr , 0);
+//            Arrays.fill(freqArr , 0);
+//            Arrays.fill(outputArr, 0);
+//            for(int j = 0 ; j < arr.length ; j++){
+//                currentDigit =(int) (arr[j] / Math.pow( 10, i)) % 10;
+////                System.out.println("Number is "+ arr[j] + " for i = " + i + " num is "+currentDigit);
+//                freqArr[currentDigit]+=1;
+//            }
+//            int k=0;
+//            int j=0;
+//        }
+    }
+
