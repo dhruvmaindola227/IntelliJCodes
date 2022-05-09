fdacb2503b1b17dbeaceae862741ad0925f0af67
dhruvmaindola227 <maindola.dhruv@gmail.com>
2022-05-09 21:47:55 +0530

    factorial with iteration(loops)

--- first project java/src/OopsPlacementAssignment/FactorialWithoutRecursion.java
+++ first project java/src/OopsPlacementAssignment/FactorialWithoutRecursion.java
@@ -1,4 +1,20 @@
 package OopsPlacementAssignment;
 
+import java.util.Scanner;
+
 public class FactorialWithoutRecursion {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number you want factorial of -->>");
+        int number = sc.nextInt();
+        factorial(number);
+    }
+
+    public static void  factorial(int number) {
+        int factorial = 1;
+        for (int i = number; i > 0; i--) {
+            factorial*= i;
+        }
+        System.out.println("Factorial of " + number + " is -> "+ factorial);
+    }
 }
