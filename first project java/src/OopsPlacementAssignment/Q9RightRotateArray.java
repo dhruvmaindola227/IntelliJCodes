ccc2a76104b1885c438591f0010628098c41faa1
dhruvmaindola227 <maindola.dhruv@gmail.com>
2022-05-11 18:56:43 +0530

    right rotate the array added

---
+++ first project java/src/OopsPlacementAssignment/Q9RightRotateArray.java
@@ -0,0 +1,33 @@
+package OopsPlacementAssignment;
+
+import java.util.Arrays;
+
+public class Q9RightRotateArray {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7,8,9,10};
+        System.out.println("original array : " + Arrays.toString(arr));
+        int numberOfTimesToRotate = 3;
+        rotate(arr , numberOfTimesToRotate);
+
+        System.out.println("Array after rotating "+numberOfTimesToRotate+" times : " + Arrays.toString(arr));
+    }
+
+    public static void reverse(int[] nums, int left, int right) { //Method made to reverse subarrays.
+        //For example. [1,2,3,4,5,6,7] reverse (1,2,3,4) and (5,6,7)
+        //in amongst themselves -> (4,3,2,1) and (7,6,5)
+        //reverse the entire array now - > [5,6,8,1,2,3,4]
+        while (left < right) {
+            int temp = nums[left];
+            nums[left++] = nums[--right];
+            nums[right] = temp;
+        }
+    }
+
+    public static void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = (n == 0 ? 0 : k % n);  //k%n is for cases where k>n
+        reverse(nums, 0    , n - k);  //reversing 1,2,3,4
+        reverse(nums, n - k, n    );   //reversing 5,6,7
+        reverse(nums, 0    , n    );   //reversing 4321765
+    }
+}
