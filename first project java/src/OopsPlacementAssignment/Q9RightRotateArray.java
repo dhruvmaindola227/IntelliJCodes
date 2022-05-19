7299d3348d919ddef372dde278e667881f512807
dhruvmaindola227 <maindola.dhruv@gmail.com>
2022-05-19 21:26:53 +0530

    added two new methods to rotate array.

--- first project java/src/OopsPlacementAssignment/Q9RightRotateArray.java
+++ first project java/src/OopsPlacementAssignment/Q9RightRotateArray.java
@@ -4,12 +4,14 @@ import java.util.Arrays;
 
 public class Q9RightRotateArray {
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5,6,7,8,9,10};
+        int[] arr = {1,2,3,4,5,6};
         System.out.println("original array : " + Arrays.toString(arr));
         int numberOfTimesToRotate = 3;
-        rotate(arr , numberOfTimesToRotate);
+//        rotate(arr , numberOfTimesToRotate);
+          rotateMyWay(arr,numberOfTimesToRotate);
+        System.out.println("Array after rotating "+numberOfTimesToRotate+" times : " +
+                Arrays.toString(arr));
 
-        System.out.println("Array after rotating "+numberOfTimesToRotate+" times : " + Arrays.toString(arr));
     }
 
     public static void reverse(int[] nums, int left, int right) { //Method made to reverse subarrays.
@@ -22,7 +24,7 @@ public class Q9RightRotateArray {
             nums[right] = temp;
         }
     }
-
+    //method number 1.
     public static void rotate(int[] nums, int k) {
         int n = nums.length;
         k = (n == 0 ? 0 : k % n);  //k%n is for cases where k>n
@@ -30,4 +32,23 @@ public class Q9RightRotateArray {
         reverse(nums, n - k, n    );   //reversing 5,6,7
         reverse(nums, 0    , n    );   //reversing 4321765
     }
+    //method number 2
+    public static int[] rotateNormal(int[] nums , int k) {
+        int[] arr = new int[nums.length];
+        for (int i = 0; i < arr.length; i++) {
+            arr[(i + k) % arr.length] = nums[i];
+        }
+        return arr;
+    }
+    //method number 3
+    public static void rotateMyWay(int[] nums , int k){
+        for (int i = 0; i < k; i++) {
+            int temp = nums[nums.length-1];
+            for (int j = nums.length - 1; j > 0; j--) {
+                nums[j] = nums[j-1];
+            }
+            nums[0] = temp;
+        }
+    }
+
 }
