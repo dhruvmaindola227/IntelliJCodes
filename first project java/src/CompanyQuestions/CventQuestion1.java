d18899461bddb4b61626893eadcc075807b5d11d
dhruvmaindola227 <maindola.dhruv@gmail.com>
2022-06-03 22:20:43 +0530

    min char to del to make string palindrome.

--- first project java/src/CompanyQuestions/CventQuestion1.java
+++ first project java/src/CompanyQuestions/CventQuestion1.java
@@ -9,7 +9,7 @@ public class CventQuestion1 {
     public static void main(String[] args) {
         int[] numbers = {1234 , 1243 , 6542 , 6742, 2342 , 2345};
         String[] names = {"souvik" , "samarth" , "dhruv" , "rohan" , "aayush" , "ronaldo"};
-        int numberToFind = 980;
+        int numberToFind = 34;
         List<String> list = new ArrayList<>();
         for (int i = 0; i < numbers.length; i++) {
             if(String.valueOf(numbers[i]).contains(String.valueOf(numberToFind))){
@@ -23,8 +23,6 @@ public class CventQuestion1 {
             for(String name : list){
                 System.out.print(name + "  ");
             }
-
         }
-
     }
 }
--- first project java/src/CompanyQuestions/CventQuestion2.java
+++ first project java/src/CompanyQuestions/CventQuestion2.java
@@ -1,16 +1,22 @@
 package CompanyQuestions;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class CventQuestion2 {
     public static void main(String[] args) {
-        String string = "aaabbbccccdddeff";
+        String string = "aaabbbccccdddeffasafaefvsfsrsdtsedgergtsd";
         System.out.println(minCharToRemove(string));
     }
-
+    //aaabbbccccdddeff
     public static int minCharToRemove(String string) {
-        Map<Character , Integer> map = new HashMap<>();
+        Map<Character ,Integer> map = new HashMap<>();
+        ArrayList<ArrayList<Character>> list = new ArrayList<>();
+        for (int i = 0; i < 2; i++) {
+            list.add(new ArrayList<>());
+        }
         int counter = 0;
         for (int i = 0; i < string.length(); i++) {
             if(!map.containsKey(string.charAt(i))){
@@ -19,12 +25,16 @@ public class CventQuestion2 {
                 map.put(string.charAt(i),map.get(string.charAt(i)) + 1);
             }
         }
-
-        for (Map.Entry<Character , Integer> key : map.entrySet()) {
-            if (key.getValue() % 2 != 0) {
-                counter++;
+        for (int i = 0; i < string.length(); i++) {
+            if(map.get(string.charAt(i)) % 2 != 0){
+                list.get(1).add(string.charAt(i));
+            }else{
+                list.get(0).add(string.charAt(i));
             }
         }
-        return counter;
+//        return counter;
+//        System.out.println(map);
+        System.out.println(list);
+        return list.get(1).size();
     }
 }
