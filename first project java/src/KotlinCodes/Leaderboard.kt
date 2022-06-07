980c41da29be88f1526beef009bd80754474d07e
dhruvmaindola227 <maindola.dhruv@gmail.com>
2022-06-07 21:40:35 +0530

    kotlin files

---
+++ first project java/src/KotlinCodes/Leaderboard.kt
@@ -0,0 +1,6 @@
+package KotlinCodes
+
+data class Leaderboard
+(   val name : String ,
+    val level : String,
+    val highScore : String )
---
+++ first project java/src/KotlinCodes/Logic.kt
@@ -0,0 +1,7 @@
+package KotlinCodes
+    var leaderboard = Leaderboard("Dhruv" , "easy" , "7")
+    var list = ArrayList<Leaderboard>()
+
+    fun main(){
+        list.add(Leaderboard("Dhruv" , "easy" , "7"))
+    }
