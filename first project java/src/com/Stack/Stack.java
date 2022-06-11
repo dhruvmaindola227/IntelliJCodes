d35cdd8f9e6c6de09c7d7b0d1d2a2c1365adb670
dhruvmaindola227 <maindola.dhruv@gmail.com>
2022-06-11 14:28:08 +0530

    added postfix evaluation method.

--- first project java/src/com/Stack/Stack.java
+++ first project java/src/com/Stack/Stack.java
@@ -4,19 +4,19 @@ import java.util.*;
 
 public class Stack {
      static int top = -1;
-     static char[] stack = new char[2000];
-    public void push(char value){
-        if(top > 9){
+     static Object[] stack = new Object[2000];
+     static private final List<Character> chars = new ArrayList<>(List.of('+' , '-' , '/' , '*' , '^' , '(' , ')'));
+    public <T> void push(T value){
+        if(top > 1999){
             throw new StackOverflowError();
         }
          stack[++top] = value;
     }
 
-    public int pop(){
+    public <T> Object pop(){
         if(top == -1){
             throw new EmptyStackException();
         }
-
         return stack[top--];
     }
 
@@ -24,7 +24,7 @@ public class Stack {
         return top == -1;
     }
 
-    public int peek(){
+    public <T> Object peek(){
         return stack[top];
     }
 
@@ -33,11 +33,55 @@ public class Stack {
             System.out.println(stack[i]);
         }
     }
+    public int evaluatePostfix(String expression){
+        int i = 0;
+        int result = 0;
+        List<Character> chars = new ArrayList<>(List.of('+' , '-' , '/' , '*' , '^' , '(' , ')'));
+        while(i < expression.length()){
+            if(!chars.contains(expression.charAt(i))){
+                push(expression.charAt(i));
+            }else{
+                result = performOperation(expression.charAt(i));
+                push(result);
+            }
+            i++;
+        }
+        System.out.println(Arrays.toString(stack));
+        return (int)pop();
+    }
+
+    private int performOperation(char operator) {
+        int operand1 = Integer.parseInt(String.valueOf(pop()));
+//        System.out.println("op1 -> "+ operand1);
+        int operand2 = Integer.parseInt(String.valueOf(pop()));
+//        System.out.println("op2 -> "+ operand2);
+        int ans = 0;
+
+        switch (operator) {
+            case '^' -> {
+                ans =operand2 ^ operand1;
+            }
+            case '*' -> {
+                ans = operand2 * operand1;
+            }
+            case '/' -> {
+                ans = operand2 / operand1;
+            }
+            case '+' -> {
+                ans = operand2 + operand1;
+            }
+            case '-' -> {
+                ans = operand2 - operand1;
+            }
+        }
+//        System.out.println("ans ->" + ans);
+        return ans;
+    }
 
     public String infixToPostFix(String expression){
         int i = 0;
         StringBuilder postFixExpression = new StringBuilder();
-        List<Character> chars = new ArrayList<>(List.of('+' , '-' , '/' , '*' , '^' , '(' , ')'));
+//        List<Character> chars = new ArrayList<>(List.of('+' , '-' , '/' , '*' , '^' , '(' , ')'));
         Map<Character , Integer> precedence = new HashMap<>();
         precedence.put('^' , 3);
         precedence.put('*' , 2);
@@ -49,7 +93,7 @@ public class Stack {
                 postFixExpression.append(expression.charAt(i));
                 i++;
             }
-            else if (top == -1 || stack[top] == '('){
+            else if (top == -1 || stack[top].equals('(')){
                 push(expression.charAt(i));
                 i++;
             }
@@ -58,25 +102,22 @@ public class Stack {
                 i++;
             }
             else if(expression.charAt(i) == ')'){
-//                do{
-//                    postFixExpression.append((char)pop());
-//                } while(stack[top] != '(');
-                while (stack[top] != '('){
+                while (!stack[top].equals('(')){
                     postFixExpression.append((char)pop());
                 }
                 pop();
                 i++;
             }
-            else if(precedence.get(expression.charAt(i)) > precedence.get(stack[top])){
+            else if(precedence.get(expression.charAt(i)) > precedence.get((char)stack[top])){
                 push(expression.charAt(i));
                 i++;
             }
-            else if(precedence.get(expression.charAt(i)) < precedence.get(stack[top])){
+            else if(precedence.get(expression.charAt(i)) < precedence.get((char)stack[top])){
                 postFixExpression.append((char)pop());
 
             }
-            else if(precedence.get(expression.charAt(i)) == precedence.get(stack[top])){
-                if (stack[top] != '^') {
+            else if(Objects.equals(precedence.get(expression.charAt(i)), precedence.get((char) stack[top]))){
+                if (!stack[top].equals('^')) {
                     postFixExpression.append((char) pop());
                 }
                 push(expression.charAt(i));
@@ -93,6 +134,7 @@ public class Stack {
     public static void main(String[] args) {
        String s ="A+B*C/(E-F)";
        Stack s1 = new Stack();
-        System.out.println(s1.infixToPostFix(s));
+//        System.out.println(s1.infixToPostFix(s));
+        System.out.println(s1.evaluatePostfix("22*34*+")); //10+
     }
 }
