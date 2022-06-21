951823dea91347f287df097c6daa5618dc0f64c2
dhruvmaindola227 <maindola.dhruv@gmail.com>
2022-06-21 21:52:37 +0530

    same ans with array faster added.

--- first project java/src/com/leetcodeQuestions/FindAllAnagramsInString.java
+++ first project java/src/com/leetcodeQuestions/FindAllAnagramsInString.java
@@ -6,7 +6,7 @@ public class FindAllAnagramsInString {
     public static void main(String[] args) {
         String s = "aa";
         String p = "bb";
-        ArrayList<Integer> list = new ArrayList<>(findAnagrams(s, p));
+        ArrayList<Integer> list = new ArrayList<>(findAnagramsUsingArrays(s, p));
         System.out.println(list);
 //        Map<Character , Integer> pCount = new HashMap<>();
 //        Map<Character , Integer> sCount = new HashMap<>();
@@ -15,6 +15,40 @@ public class FindAllAnagramsInString {
 //        System.out.println(pCount.equals(sCount));
     }
 
+
+    public static ArrayList<Integer> findAnagramsUsingArrays(String s, String p) {
+
+        int[] freq1 = new int[26];
+        int[] freq2 = new int[26];
+        ArrayList<Integer> list = new ArrayList<>();
+
+        if(s.length()<p.length())
+            return list;
+
+        for(int i=0; i<p.length(); i++){
+            freq1[s.charAt(i)-'a']++;
+            freq2[p.charAt(i)-'a']++;
+        }
+
+        int start=0;
+        int end=p.length();
+
+        if(Arrays.equals(freq1,freq2))
+            list.add(start);
+
+        while(end<s.length()){
+
+            freq1[s.charAt(start)-'a']--;
+            freq1[s.charAt(end)-'a']++;
+
+            if(Arrays.equals(freq1,freq2))
+                list.add(start+1);
+
+            start++;
+            end++;
+        }
+        return list;
+    }
 
     public static List<Integer> findAnagrams(String s, String p) {
         ArrayList<Integer> answer = new ArrayList<>();
