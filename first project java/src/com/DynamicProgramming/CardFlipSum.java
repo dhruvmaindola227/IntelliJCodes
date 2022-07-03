d22463f8af27c8b7da43413de9e247faf4002bd5
Dhruv <dhruv.maindola227>
2022-07-03 22:25:45 +0530

    minimum subarray kadanes basically

---
+++ first project java/src/com/DynamicProgramming/CardFlipSum.java
@@ -0,0 +1,31 @@
+package com.DynamicProgramming;
+
+public class CardFlipSum {
+    public static void main(String[] args) {
+        int size = 5;
+        int[] cards = {-100,2,3,4,-5};
+        int sum = 0;
+        for (int card : cards) {
+            sum += card;
+        }
+        //removing and adding the positive version of the minimum sum subarray will give us the maximum sum.
+        System.out.println((maxSumPossible(size , cards) * -2) + sum);
+
+    }
+
+    public static int maxSumPossible(int size, int[] cards) {
+        //kadanes algorithm to find minimum sum subarray.
+        int min_sum = Integer.MAX_VALUE;
+        int curr_sum = 0;
+        for (int i = 0 ; i < size; i++) {
+            curr_sum = curr_sum + cards[i];
+            if (curr_sum < min_sum)
+                min_sum = curr_sum;
+            if (curr_sum > 0)
+                curr_sum  = 0;
+        }
+        return min_sum;
+    }
+
+
+}
