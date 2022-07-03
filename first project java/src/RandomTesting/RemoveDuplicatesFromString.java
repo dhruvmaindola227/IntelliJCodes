c0cfaff5793fbbdfc4a486fe43395af97e59f26f
Dhruv <dhruv.maindola227>
2022-07-03 12:29:50 +0530

    geeksforgeeks question

---
+++ first project java/src/RandomTesting/RemoveDuplicatesFromString.java
@@ -0,0 +1,23 @@
+package RandomTesting;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class RemoveDuplicatesFromString {
+    public static void main(String[] args) {
+        String s= "geeksforgeeks";
+        System.out.println(removeDuplicates(s));
+    }
+
+    public static String removeDuplicates(String s) {
+        StringBuilder sb = new StringBuilder();
+        Set<Character> set = new HashSet<>();
+        for (int i = 0; i < s.length(); i++) {
+            if(!set.contains(s.charAt(i))){
+                set.add(s.charAt(i));
+                sb.append(s.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+}
