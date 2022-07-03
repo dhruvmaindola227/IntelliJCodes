4e121ff23ede1fb6907618c31c297d95cd1bd057
Dhruv <dhruv.maindola227>
2022-07-03 12:33:01 +0530

    psvm edited

--- first project java/src/com/DynamicProgramming/MaximizeBookPoints.java
+++ first project java/src/com/DynamicProgramming/MaximizeBookPoints.java
@@ -2,10 +2,11 @@ package com.DynamicProgramming;
 
 public class MaximizeBookPoints {
     public static void main(String[] args) {
-//        System.out.println(maximizePoints(3, 7 , new int[]{2,6,9} , new int[]{3,5,3}));
-        int a = 10;
-        System.out.println();
+        System.out.println(maximizePoints(3, 7, new int[]{2, 6, 9}, new int[]{3, 5, 3}));
+
     }
+
+
     public static int maximizePoints(int size , int maxDuration , int[] points , int[] bookDuration){
        if(maxDuration == 0 || size == 0){ //base condtion -> smallest valid input.
            return 0;
