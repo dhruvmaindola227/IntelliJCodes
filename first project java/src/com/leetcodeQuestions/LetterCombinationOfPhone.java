e97ce80bc5340db4ae1fd2c8304aac1c8030d0cc
Dhruv <dhruv.maindola227>
2022-07-05 21:36:47 +0530

    added non hashmap method. Doesnt work for 7 and 9

--- first project java/src/com/leetcodeQuestions/LetterCombinationOfPhone.java
+++ first project java/src/com/leetcodeQuestions/LetterCombinationOfPhone.java
@@ -4,44 +4,58 @@ import java.util.*;
 
 public class LetterCombinationOfPhone {
     public static void main(String[] args) {
-        String digits = "23";
+        String digits = "79";
         System.out.println(letterCombinations(digits));
+        System.out.println(letterCombinationsWithoutMap("",digits));
     }
 
-        public static List<String> letterCombinations(String digits) {
-            ArrayList<String> ans = new ArrayList<>();
-            if(digits.length() == 0){
-                return new ArrayList<>();
-            }
-            HashMap<Character , String> map = new HashMap<>();
-            map.put('2',"abc");
-            map.put('3',"def");
-            map.put('4',"ghi");
-            map.put('5',"jkl");
-            map.put('6',"mno");
-            map.put('7',"pqrs");
-            map.put('8',"tuv");
-            map.put('9',"wxyz");
-            ans = helper("" , digits , map);
-            return ans;
+    public static List<String> letterCombinations(String digits) {
+        ArrayList<String> ans = new ArrayList<>();
+        if(digits.length() == 0){
+            return new ArrayList<>();
         }
+        HashMap<Character , String> map = new HashMap<>();
+        map.put('2',"abc");
+        map.put('3',"def");
+        map.put('4',"ghi");
+        map.put('5',"jkl");
+        map.put('6',"mno");
+        map.put('7',"pqrs");
+        map.put('8',"tuv");
+        map.put('9',"wxyz");
+        ans = helper("" , digits , map);
+        return ans;
+    }
 
-        public static ArrayList<String> helper(String p , String up , HashMap<Character , String> charMap){
-            ArrayList<String> ans = new ArrayList<>();
-            ArrayList<String> temp = new ArrayList<>();
-            //base condition
-            if(up == ""){
-                ans.add(p);
-                return ans;
-            }
-            //using backtracking (processed and unprocessed approach).
-            char firstDigit = up.charAt(0);
-            String alphabets = charMap.get(firstDigit);
-            for(int i = 0 ; i < alphabets.length() ; i++){
-                temp =  helper(p + alphabets.charAt(i) , up.substring(1) , charMap);
-                ans.addAll(temp);
-            }
+    public static ArrayList<String> helper(String p , String up , HashMap<Character , String> charMap){
+        ArrayList<String> ans = new ArrayList<>();
+        //base condition
+        if(up == ""){
+            ans.add(p);
             return ans;
         }
+        //using backtracking (processed and unprocessed approach).
+        char firstDigit = up.charAt(0);
+        String alphabets = charMap.get(firstDigit);
+        for(int i = 0 ; i < alphabets.length() ; i++){
+            ans.addAll(helper(p + alphabets.charAt(i) , up.substring(1) , charMap));
+        }
+        return ans;
+    }
+
+    public static ArrayList<String> letterCombinationsWithoutMap(String p,String digits) {
+        ArrayList<String> ans = new ArrayList<>();
+        //base condition
+        if(digits == ""){
+            ans.add(p);
+            return ans;
+        }
+        int current_digit = digits.charAt(0) - '0';
+        for (int i = ((current_digit - 1) * 3) - 3; i < (current_digit - 1) * 3; i++) {
+            char ch = (char) ('a' + i);
+            ans.addAll(letterCombinationsWithoutMap(p + ch , digits.substring(1)));
+        }
+        return ans;
+    }
     }
 
