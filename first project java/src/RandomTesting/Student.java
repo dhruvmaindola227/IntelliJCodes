6808de3eb2ab9186fd822b97836e31ed30722603
Dhruv <maindola.dhruv@gmail.com>
2022-08-10 01:50:19 +0530

    new files

---
+++ first project java/src/RandomTesting/Student.java
@@ -0,0 +1,55 @@
+package RandomTesting;
+
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class AayushTesting {
+//    public static String toString(int[] arr) {
+//        StringBuilder sb = new StringBuilder();
+//        sb.append("[ ");
+//        for (int i = 0; i < arr.length; i++) {
+//            sb.append(arr[i]).append(", ");
+//            if(i == arr.length - 1){
+//                return sb.append(" ]").toString();
+//            }
+//        }
+//        return sb.append(" ]").toString();
+//    }
+
+    public static void main(String[] args) {
+//        int[][] arr_2d = {{0,1} , {0,1} , {0,1}};
+//        int[] arr = {1,2,3,4,5};
+//        int[] result = printArr(arr);
+//        ArrayList<ArrayList<Integer>> list_2d = new ArrayList<>();
+//        for (int i = 0; i < 3; i++) {
+//            list_2d.add(i , new ArrayList<>());
+//        }
+//        for(ArrayList<Integer> list : list_2d){
+//            for (int i = 0; i < 2; i++) {
+//                list.add(i);
+//            }
+//        }
+//        System.out.println("ArrayList class's toString() method-> " + list_2d);
+//
+//
+////        for (int raju : result) {
+////            System.out.print(raju + " ");
+////        }
+////        System.out.println("Arrays class implement " + Arrays.toString(arr));
+////        reverseArray(arr);
+////        System.out.println("Arrays class implement " + Arrays.toString(arr));
+//        System.out.print("[");
+//        for(int[] nums : arr_2d){
+//            System.out.print(Arrays.toString(nums) + ",");
+//        }
+//        System.out.print(" ]");
+        Map<Integer , Integer> map = new TreeMap<>();
+        map.put(24,2);
+        map.put(3,4);
+        System.out.println(map);
+
+    }
+
+
+    }
+
---
+++ first project java/src/com/Heap/LastStoneWeight.java
@@ -0,0 +1,2 @@
+package com.Heap;public class LastStoneWeight {
+}
