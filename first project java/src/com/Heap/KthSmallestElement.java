c04821f2122f57879442e5f2338d8202f818903b
Dhruv <maindola.dhruv@gmail.com>
2022-08-21 22:41:54 +0530

    minor changes

--- first project java/src/com/Heap/KthSmallestElement.java
+++ first project java/src/com/Heap/KthSmallestElement.java
@@ -1,10 +1,11 @@
 package com.Heap;
 
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class KthSmallestElement {
     public static void main(String[] args) {
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((o1 , o2) -> o2 - o1); //this is max heap.
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((Comparator.reverseOrder())); //this is max heap.
         PriorityQueue<Integer> minHeap = new PriorityQueue<>(); //this is min heap by default.
         int[] arr = {1,2,3,3,4,5,5,6,6,6,6,6,6,6};
         System.out.println(findKthLargest(arr , 2));
