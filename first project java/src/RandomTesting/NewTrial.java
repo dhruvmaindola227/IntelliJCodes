6964af0583cf6573053a813bceb479c0e02ee717
Dhruv <maindola.dhruv@gmail.com>
2022-08-22 12:48:45 +0530

    new random files

---
+++ first project java/src/RandomTesting/NewTrial.java
@@ -0,0 +1,65 @@
+package RandomTesting;
+
+import java.util.Objects;
+
+//static factory methods.
+//director@bciit.ac.in
+public class NewTrial {
+    public static void main(String[] args) {
+    StringBuffer sb = new StringBuffer("rohan");
+    StringBuffer sb1 = new StringBuffer("rohan");
+        System.out.println(sb == sb1);
+        System.out.println(sb.equals(sb1));
+        System.out.println(sb.toString().equals(sb1.toString()));
+    }
+}
+
+class Account{
+    public Account() {
+    }
+
+    private int accNo;
+    private String accOwner;
+
+    public int getAccNo() {
+        return accNo;
+    }
+
+    public Account setAccNo(int accNo) {
+        this.accNo = accNo;
+        return this;
+    }
+
+    public String getAccOwner() {
+        return accOwner;
+    }
+
+    public void setAccOwner(String accOwner) {
+        this.accOwner = accOwner;
+    }
+
+    public Account(int accNo, String accOwner) {
+        this.accNo = accNo;
+        this.accOwner = accOwner;
+    }
+}
+
+class AccountService{
+    private static AccountService accountService;
+    private AccountService() {
+
+    }
+
+    public static AccountService getAccountServiceObject(){
+        if(accountService == null){
+            accountService = new AccountService();
+        }
+        return accountService;
+    }
+
+
+
+    }//instance method.
+
+
+
--- first project java/src/RandomTesting/TypeCasting.java
+++ first project java/src/RandomTesting/TypeCasting.java
@@ -1,9 +1,20 @@
 package RandomTesting;
 
 public class TypeCasting {
+
+    public static void callMe(){
+        System.out.println("cm");
+    }
+
     public static void main(String[] args) {
         String s = "dasd";
         Object o = (Object) s;
         System.out.println(s == o);
+    }
+}
+
+class newClass extends TypeCasting{
+    public static void callMe(){
+        TypeCasting.callMe();
     }
 }
