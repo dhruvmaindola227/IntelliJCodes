d6d528fcf04b4225db7bf919418dfb7634d396b9
Dhruv <maindola.dhruv@gmail.com>
2022-08-23 01:17:44 +0530

    new files

--- first project java/src/RandomTesting/MethodOverriding.java
+++ first project java/src/RandomTesting/MethodOverriding.java
@@ -1,17 +1,24 @@
 package RandomTesting;
 
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class MethodOverriding {
     public static void main(String[] args) {
-        System.out.println(createString(new char[]{'a' , 'b'}));
+        ArrayList<Integer> list = new ArrayList<>(List.of(1,2));
+        System.out.println(list);
+
     }
 
     public static String reverse(String s) {
-        return new StringBuilder(s).reverse().toString();
+       return new StringBuilder(s).reverse().toString();
     }
 
     public static String createString(char[] chars){
         StringBuilder s = new StringBuilder();
-        for(char ch : chars){
+        for (char ch : chars){
             s.append(ch);
         }
         return s.toString();
