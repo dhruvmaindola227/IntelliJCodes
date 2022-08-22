3b93179f67e05270d5b126dc5168dc12e0c14b3a
Dhruv <maindola.dhruv@gmail.com>
2022-08-22 07:28:56 +0530

    new filess

---
+++ first project java/src/RandomTesting/TypeCasting.java
@@ -0,0 +1,9 @@
+package RandomTesting;
+
+public class TypeCasting {
+    public static void main(String[] args) {
+        String s = "dasd";
+        Object o = (Object) s;
+        System.out.println(s == o);
+    }
+}
