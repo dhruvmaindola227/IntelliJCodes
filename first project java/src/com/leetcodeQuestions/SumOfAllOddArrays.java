d31ec4d39ec9689917d0c20db9d2a0e7dfd055ae
Dhruv <maindola.dhruv@gmail.com>
2022-10-02 15:16:24 +0530

    optimized the sliding window sum

--- first project java/src/com/leetcodeQuestions/SumOfAllOddArrays.java
+++ first project java/src/com/leetcodeQuestions/SumOfAllOddArrays.java
@@ -2,31 +2,32 @@ package com.leetcodeQuestions;
 
 public class SumOfAllOddArrays {
     public static void main(String[] args) {
-        System.out.println(sumOddLengthSubarrays(new int[] {1,2,3,4,5}));
+        System.out.println(sumOddLengthSubarrays(new int[] {1,4,2,5,3}));
     }
     public static int sumOddLengthSubarrays(int[] arr) {
         int windowSize = 1;
-        int currentMax = 0;
+        int currentSum = 0;
         int finalSum = 0;
 
             for (windowSize = 1; windowSize <= arr.length ; windowSize+=2) {
-                currentMax = slidingWindowMaxOfSizeK(arr , windowSize);
-                finalSum += currentMax;
+                currentSum = sumOfAllSlidingWindowOfSizeK(arr , windowSize);
+                finalSum += currentSum;
             }
 
         return finalSum;
     }
-    public static int slidingWindowMaxOfSizeK(int[] arr , int k ) {
+    public static int sumOfAllSlidingWindowOfSizeK(int[] arr , int k ) {
         int currSum = 0;
         int absSum = 0;
-        int max = Integer.MIN_VALUE;
-        for (int i = 0; i <= arr.length - k; i++) {
-            currSum = 0;
-            for (int j = i; j < i + k; j++) {
+        //[1,]
+            for (int j = 0; j < k; j++) {
                 currSum += arr[j];
             }
             absSum += currSum;
-        }
+            for (int j = k; j < j + k && j < arr.length; j++) {
+                currSum = currSum - arr[j - k] + arr[j];
+                absSum += currSum;
+            }
         return absSum;
     }
 }
