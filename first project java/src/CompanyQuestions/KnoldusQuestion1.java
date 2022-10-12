9011467607bec4fa8e552a04ed1f6c3afeba61ed
Dhruv <maindola.dhruv@gmail.com>
2022-10-12 21:30:26 +0530

    knoldus question

---
+++ first project java/src/CompanyQuestions/KnoldusQuestion1.java
@@ -0,0 +1,25 @@
+package CompanyQuestions;
+
+import java.util.Arrays;
+
+public class KnoldusQuestion1 {
+    //return the highest length word excluding the special characters in the words
+    public static void main(String[] args) {
+        String s = "Hello^*&%*&oooooo! To the world of Ques*)*^tions";
+        System.out.println("length is => " + lengthOfWordExcludingSplChars(s));
+    }
+
+    public static int lengthOfWordExcludingSplChars(String s){
+        String[] words = s.split("[\\s]+"); //split using 1 or more spaces
+        int maxLength = Integer.MIN_VALUE;
+        int lengthOfString = 0;
+        for(String word : words){
+            lengthOfString = word.replaceAll("[^a-zA-Z]" , "").length();
+            //replace all characters except a to z or A to Z with "".
+            System.out.print(word.replaceAll("[^a-zA-Z]" , "") + " ");
+            maxLength = Math.max(maxLength , lengthOfString);
+        }
+        System.out.println();
+        return maxLength;
+    }
+}
