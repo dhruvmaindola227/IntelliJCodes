44e28b38006f8c6d3e0576be090f49ea8dd933c6
Dhruv <maindola.dhruv@gmail.com>
2023-01-28 23:31:51 +0530

    new files

--- first project java/src/Teaching/NaiveStringMatching.java
+++ first project java/src/Teaching/NaiveStringMatching.java
@@ -4,11 +4,18 @@ import java.util.ArrayList;
 
 public class NaiveStringMatching {
     public static void main(String[] args) {
-        String string = "aacbcedaacvacaava";
-        String pattern = "aac";
-        System.out.println(listOfOccurences(string , pattern));
-    }
+        String string = "manjari";
+        String pattern = "Manjari";
+        if(string.contains(pattern)){
+            System.out.println(true);
+        }
 
+        System.out.println(string.hashCode());
+        System.out.println(pattern.hashCode());
+
+//        System.out.println(listOfOccurences(string , pattern));
+    }
+    //we are finding out all possible SUBSTRINGS of length of pattern from start in string.
     public static ArrayList<Integer> listOfOccurences(String string , String pattern){
         ArrayList<Integer> indices = new ArrayList<>();
         String sb = "";
